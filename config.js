0af17b7b2df58bd03f7b6f13677f4b20b30a5da4
trevoranderson <trevoranderson@users.noreply.github.com>
2014-10-18 21:08:40 -0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    uname: "USERNAME",
+    pw: "PASSWORD",
+    webroot: "../webroot", // What folder to host
+    writeroot: "../writeroot", // What folder to write files to
+};
